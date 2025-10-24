813051c8b7da35840efe126fdc892b7add96b8af
MOHAMEDMOSTAFAALLI <97741917+MOHAMEDMOSTAFAALLI@users.noreply.github.com>
2025-10-24 20:18:11 +0300

    Add files via upload

---
+++ C#-EXAM-2/C#-EXAM-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using C__EXAM_2.question;
+namespace C__EXAM_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //A[] answers = {new A(1, "Paris"), new A(2, "London"), new A(3, "Cairo")};
+
+            //MCQQuestion q1 = new MCQQuestion("Q1", "What is the capital of France?", 5, answers, 1);
+            //Console.WriteLine(q1);
+
+            TrueFalseQuestion q2 = new TrueFalseQuestion("Q2", "The Earth is flat.", 2, 2);
+            Console.WriteLine(q2);
+
+
+
+
+            // name of the class + name of the instence = new name of the calss(the data in the constructor)
+
+
+
+        }
+    }
+}
---
+++ C#-EXAM-2/C#-EXAM-2/question/E.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__EXAM_2.question
+{
+    public class E  //Exam
+    {
+        public int ExamTime;
+        public int numberOfQuestions;
+        public Q[] Questions;
+
+        public void showExam(int examTime)
+        {
+
+        }
+    }
+}
